a1eed0805a372e5dea8b68740f06f2327fc4d38b
KelvinAaron <k.aaron-onu@alustudent.com>
2025-03-14 11:23:16 -0700

    done

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
-st all records of the table second table.
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- List all records of second table.
+SELECT score, name FROM second_table ORDER BY score DESC
